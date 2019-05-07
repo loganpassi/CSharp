32bf483623efb40864a75e8cdb764ea3684414aa
Logan Passi <50377116+loganpassi@users.noreply.github.com>
2019-05-07 18:41:17 -0500

    Add files via upload

---
+++ SnailRacer/SnailRacer/SnailRacer/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace SnailRacer
+{
+    public partial class Form1 : Form
+    {
+
+        List<Snail> snails = new List<Snail>();
+        SoundPlayer vroom = new SoundPlayer(Properties.Resources.vroom);
+
+        public Form1()
+        {
+            const int StartX = 50;
+            InitializeComponent();
+            Snail s = new Snail(StartX, 75, Properties.Resources.snail1);
+            snails.Add(s);
+            s = new Snail(StartX, 150, Properties.Resources.snail2);
+            snails.Add(s);
+            s = new Snail(StartX, 250, Properties.Resources.snail3);
+            snails.Add(s);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(Snail s in snails)
+            {
+                s.Draw(e.Graphics);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach(Snail s in snails)
+            {
+                s.Move();
+                if(s.X >= 750)
+                {
+                    timer1.Stop();
+                }
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(Snail s in snails)
+            {
+                s.X = 50;
+            }
+            vroom.Play();
+            timer1.Start();
+        }
+    }
+}
---
+++ SnailRacer/SnailRacer/SnailRacer/Snail.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Drawing;
+
+namespace SnailRacer
+{
+    class Snail
+    {
+        Image image;
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        static Random r = new Random();
+        public Snail()
+        {
+            X = 0;
+            Y = 0;
+            image = Properties.Resources.snail1;
+        }
+        public Snail(int x, int y, Image image)
+        {
+            X = x;
+            Y = y;
+            this.image = image;
+        }
+
+        public void Move()
+        {
+            X += r.Next(10, 20);
+            
+        }
+
+        public void Draw(Graphics g)
+        {
+            g.DrawImage(image, X - image.Width/2, Y - image.Width/2);
+        }
+    }
+    class TurboSnail : Snail { }
+}
