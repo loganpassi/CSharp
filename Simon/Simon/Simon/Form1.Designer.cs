e768a998de611ed6254a5acec0ff2403d5e9f163
Logan Passi <50377116+loganpassi@users.noreply.github.com>
2019-05-07 18:34:07 -0500

    Add files via upload

---
+++ Simon/Simon/Simon/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿using System;
+
+namespace Simon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxBlue = new System.Windows.Forms.PictureBox();
+            this.pictureBoxYellow = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRed = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGreen = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testToolStripToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testLightsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYellow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxBlue
+            // 
+            this.pictureBoxBlue.Image = global::Simon.Properties.Resources.img_blue_off;
+            this.pictureBoxBlue.Location = new System.Drawing.Point(21, 225);
+            this.pictureBoxBlue.Name = "pictureBoxBlue";
+            this.pictureBoxBlue.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxBlue.TabIndex = 3;
+            this.pictureBoxBlue.TabStop = false;
+            this.pictureBoxBlue.Click += new System.EventHandler(this.pictureBoxBlue_Click);
+            // 
+            // pictureBoxYellow
+            // 
+            this.pictureBoxYellow.Image = global::Simon.Properties.Resources.img_yellow_off;
+            this.pictureBoxYellow.Location = new System.Drawing.Point(218, 225);
+            this.pictureBoxYellow.Name = "pictureBoxYellow";
+            this.pictureBoxYellow.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxYellow.TabIndex = 2;
+            this.pictureBoxYellow.TabStop = false;
+            this.pictureBoxYellow.Click += new System.EventHandler(this.pictureBoxYellow_Click);
+            // 
+            // pictureBoxRed
+            // 
+            this.pictureBoxRed.Image = global::Simon.Properties.Resources.img_red_off;
+            this.pictureBoxRed.Location = new System.Drawing.Point(218, 28);
+            this.pictureBoxRed.Name = "pictureBoxRed";
+            this.pictureBoxRed.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxRed.TabIndex = 1;
+            this.pictureBoxRed.TabStop = false;
+            this.pictureBoxRed.Click += new System.EventHandler(this.pictureBoxRed_Click);
+            // 
+            // pictureBoxGreen
+            // 
+            this.pictureBoxGreen.Image = global::Simon.Properties.Resources.img_green_off;
+            this.pictureBoxGreen.Location = new System.Drawing.Point(21, 28);
+            this.pictureBoxGreen.Name = "pictureBoxGreen";
+            this.pictureBoxGreen.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxGreen.TabIndex = 0;
+            this.pictureBoxGreen.TabStop = false;
+            this.pictureBoxGreen.Click += new System.EventHandler(this.pictureBoxGreen_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(501, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripToolStripMenuItem,
+            this.testLightsToolStripMenuItem,
+            this.restartToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // testToolStripToolStripMenuItem
+            // 
+            this.testToolStripToolStripMenuItem.Name = "testToolStripToolStripMenuItem";
+            this.testToolStripToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.testToolStripToolStripMenuItem.Text = "TestToolStrip";
+            this.testToolStripToolStripMenuItem.Click += new System.EventHandler(this.testToolStripToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // testLightsToolStripMenuItem
+            // 
+            this.testLightsToolStripMenuItem.Name = "testLightsToolStripMenuItem";
+            this.testLightsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.testLightsToolStripMenuItem.Text = "Test Lights";
+            this.testLightsToolStripMenuItem.Click += new System.EventHandler(this.testLightsToolStripMenuItem_Click);
+            // 
+            // restartToolStripMenuItem
+            // 
+            this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            this.restartToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.restartToolStripMenuItem.Text = "Restart";
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 456);
+            this.Controls.Add(this.pictureBoxBlue);
+            this.Controls.Add(this.pictureBoxYellow);
+            this.Controls.Add(this.pictureBoxRed);
+            this.Controls.Add(this.pictureBoxGreen);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Simon";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYellow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxGreen;
+        private System.Windows.Forms.PictureBox pictureBoxRed;
+        private System.Windows.Forms.PictureBox pictureBoxYellow;
+        private System.Windows.Forms.PictureBox pictureBoxBlue;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testLightsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+    }
+}
+
---
+++ Simon/Simon/Simon/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace Simon
+{
+    enum Colors { Red, Blue, Yellow, Green, Buzzer }
+
+
+    public partial class Form1 : Form
+    {
+        SoundPlayer redSound = new SoundPlayer(Properties.Resources.snd_a_High);
+        SoundPlayer blueSound = new SoundPlayer(Properties.Resources.snd_a_low);
+        SoundPlayer yellowSound = new SoundPlayer(Properties.Resources.snd_middle_c);
+        SoundPlayer greenSound = new SoundPlayer(Properties.Resources.snd_E);
+        SoundPlayer buzzerSound = new SoundPlayer(Properties.Resources.snd_buzzer);
+
+        int currIndex = 0;
+        List<Colors> sequence = new List<Colors>();
+        Dictionary<Colors, PictureBox> buttonMap = new Dictionary<Colors, PictureBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonMap[Colors.Red] = pictureBoxRed;
+            buttonMap[Colors.Blue] = pictureBoxBlue;
+            buttonMap[Colors.Green] = pictureBoxGreen;
+            buttonMap[Colors.Yellow] = pictureBoxYellow;
+        }
+
+        private void pictureBoxYellow_Click(object sender, EventArgs e)
+        {
+            // FlashButton(pictureBoxYellow, Colors.Yellow);
+            HandleButton(Colors.Yellow);
+
+        }
+
+        private void pictureBoxBlue_Click(object sender, EventArgs e)
+        {
+            // FlashButton(pictureBoxBlue, Colors.Blue);
+            HandleButton(Colors.Blue);
+
+        }
+
+        private void pictureBoxGreen_Click(object sender, EventArgs e)
+        {
+            //FlashButton(pictureBoxGreen, Colors.Green);
+            HandleButton(Colors.Green);
+        }
+
+        private void pictureBoxRed_Click(object sender, EventArgs e)
+        {
+            //FlashButton(pictureBoxRed, Colors.Red);
+            HandleButton(Colors.Red);
+
+        }
+
+        void LightOn(PictureBox pb, Colors c)
+        {
+            if(c == Colors.Red)
+            {
+                pb.Image = Properties.Resources.img_red_on;
+            }
+            else if (c == Colors.Green)
+            {
+                pb.Image = Properties.Resources.img_green_on;
+            }
+            else if (c == Colors.Blue)
+            {
+                pb.Image = Properties.Resources.img_blue_on;
+            }
+            else if (c == Colors.Yellow)
+            {
+                pb.Image = Properties.Resources.img_yellow_on;
+            }
+            pb.Refresh();
+        }
+        void LightOff(PictureBox pb, Colors c)
+        {
+            if (c == Colors.Red)
+            {
+                pb.Image = Properties.Resources.img_red_off;
+            }
+            else if (c == Colors.Green)
+            {
+                pb.Image = Properties.Resources.img_green_off;
+            }
+            else if (c == Colors.Blue)
+            {
+                pb.Image = Properties.Resources.img_blue_off;
+            }
+            else if (c == Colors.Yellow)
+            {
+                pb.Image = Properties.Resources.img_yellow_off;
+            }
+
+            pb.Refresh();
+        }
+        void PlaySound(Colors c)
+        {
+            if(c == Colors.Red)
+            {
+                redSound.PlaySync();
+            }
+            else if(c == Colors.Yellow)
+            {
+                yellowSound.PlaySync();
+            }
+            else if(c == Colors.Blue)
+            {
+                blueSound.PlaySync();
+            }
+            else if(c == Colors.Green)
+            {
+                greenSound.PlaySync();
+            }
+            else if (c == Colors.Buzzer)
+            {
+                buzzerSound.PlaySync();
+            }
+        }
+
+        private void testToolStripToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PlaySound(Colors.Green);
+            PlaySound(Colors.Blue);
+            PlaySound(Colors.Yellow);
+            PlaySound(Colors.Red);
+            PlaySound(Colors.Buzzer);
+        }
+
+        void FlashButton(PictureBox pb, Colors c)
+        {
+            //turn on button
+            LightOn(pb, c);
+            //play sound
+            PlaySound(c);
+            //turn off button
+            LightOff(pb, c);
+        }
+        private void testLightsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FlashButton(pictureBoxBlue, Colors.Blue);
+            FlashButton(pictureBoxYellow, Colors.Yellow);
+            FlashButton(pictureBoxRed, Colors.Red);
+            FlashButton(pictureBoxGreen, Colors.Green);
+        }
+
+        //puts a light in the sequence;
+        void AddLight()
+        {
+            Random r = new Random();
+            int next = r.Next(0, 3);
+            Colors c = (Colors)next;
+
+            sequence.Add((Colors)next);
+        }
+
+        
+
+        void PlaySequence()
+        {
+            Thread.Sleep(500);
+            foreach(Colors c in sequence)
+            {
+                FlashButton(buttonMap[c], c);
+                Thread.Sleep(200);
+            }
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Restart();
+        }
+
+        void Restart()
+        {
+            sequence.Clear();
+            currIndex=0;
+            AddLight();
+            PlaySequence();
+        }
+
+
+        void HandleButton(Colors c)
+        {
+            if(sequence.Count > 0)
+            {
+                if(c== sequence[currIndex])
+                {
+                    FlashButton(buttonMap[c], c);
+                    currIndex++; //go to the next button
+
+                    //did the player get to the end?
+                    if(currIndex == sequence.Count)
+                    {
+                        AddLight();
+                        currIndex = 0;
+                        PlaySequence();
+                    }
+                }
+                else
+                {
+                    PlaySound(Colors.Buzzer);
+                    Restart();
+                }
+            }
+            else
+            {
+                Restart();
+            }
+        }
+    }
+}
