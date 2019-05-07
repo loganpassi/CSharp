b4dfb4a06e61e8a7ade6e858f8b5f8e763dec89f
Logan Passi <50377116+loganpassi@users.noreply.github.com>
2019-05-07 18:31:52 -0500

    Add files via upload

---
+++ e/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float e = 1F, index = 1F, den = 1F;
+            const int SIZE = 1000;
+            while(index < SIZE + 1)
+            {
+                for(float i = 1F; i < index + 1F; i++)
+                {
+                    den *= i;
+                }
+                e += 1 / den;
+                den = 1F;
+                index++;
+            }
+            Console.WriteLine("e = " + e);
+        }
+    }
+}
