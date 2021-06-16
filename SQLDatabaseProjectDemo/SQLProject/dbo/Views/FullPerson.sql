10307192504172f6fa84006e081a59e6272043d1
Logan Passi <loganpassi88@gmail.com>
2021-06-15 20:15:03 -0500

    Project to host small local SQL database

---
+++ SQLDatabaseProjectDemo/SQLProject/dbo/Views/FullPerson.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[FullPerson]
+	AS SELECT [p].[PersonId] as PersonId, [p].[FirstName], [p].[LastName],
+			  [a].[Id] as AddressId, [a].[StreetAddress], [a].[City], [a].[State], [a].[ZipCode] 
+	FROM dbo.Person p
+
+	left join dbo.Address
+	a on p.[PersonId] = a.PersonId
